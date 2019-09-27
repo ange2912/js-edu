3010910b48ea0ec65c66fe4e055d2cbb7790504d
Ange <genachumakov@mail.ru>
2019-09-27 15:56:44 +0300

    feat: add my solution

--- src/index.js
+++ src/index.js
@@ -9,6 +9,58 @@ module.exports = function getTimeForEducation(
     knowsProgramming = true,
     config = {family: 4}
     ) {
-      return 0;
+      let weeks;
+
+      if (focus === 'family' && knowsProgramming){
+        weeks=800/config.family;
+      } 
+      else if (
+        focus === 'family' && !knowsProgramming){
+          weeks=1300/config.family;
+        }
+
+      if (focus === 'friends' && knowsProgramming){
+        weeks=800/config.friends;
+      } 
+      else if (
+        focus === 'friends' && !knowsProgramming){
+          weeks=1300/config.friends;
+        }
+
+
+      if (focus === 'normal_life' && knowsProgramming){
+        weeks=800/config.normal_life;
+      } 
+      else if (
+        focus === 'normal_life' && !knowsProgramming){
+          weeks=1300/config.normal_life;
+        }
+
+      if (focus === 'profession' && knowsProgramming){
+        weeks=800/config.profession;
+      } 
+      else if (
+        focus === 'profession' && !knowsProgramming){
+          weeks=1300/config.profession;
+        }
+
+      if (focus === 'carrier' && knowsProgramming){
+        weeks=800/config.carrier;
+      } 
+      else if (
+        focus === 'carrier' && !knowsProgramming){
+          weeks=1300/config.carrier;
+        }
+
+      if (focus === 'top_peformance' && knowsProgramming){
+        weeks=800/config.top_peformance;
+      } 
+      else if (
+        focus === 'top_peformance' && !knowsProgramming){
+          weeks=1300/config.top_peformance;
+        }
+      
+      weeks = Math.ceil(weeks);
+      return weeks;
   };
-  
+    
